4110a6b31f157d693914f4cacd8130f7e7a23b2a
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-17 12:33:56 -0400

    Updated from Brython Server: 9/17/2018 12:33:54 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,13 +25,15 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+def tnirp(b):
+    print(b[::-1])
+#reverse the text
 string = input("Please enter a string of text (the bigger the better): ")
 words = list(string)
 n = (len(string))
 for n in range(1, n + 1):
-    revcor = list()
-def tnirp(b):
-    print(b[::-1])
+    revcor = list(n)
+
 n = (len(string))
 for n in range(1,len(string) + 1):
     #revcor[n] = words[n]
