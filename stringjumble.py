9435cc00f9a9b89e4f4aff8a679e58f4b293bc9e
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-21 12:58:06 -0400

    Updated from Brython Server: 9/21/2018 12:58:04 PM

--- stringjumble.py
+++ stringjumble.py
@@ -50,7 +50,7 @@ for d in range(0,n-1):
         e = e + 1
 w = len(spaces)
 for t in range(0,w):
-    revcor[n-spaces[t]:n] = unedit[0:spaces[t]]
+    revcor[n - int(spaces[t]):n] = unedit[0:spaces[t]]
     
 print(n)
 #print(u)
