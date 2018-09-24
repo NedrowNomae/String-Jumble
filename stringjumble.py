cca9a534a2f5663cad30c19488b5562a35968286
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-24 12:35:36 -0400

    Updated from Brython Server: 9/24/2018 12:35:36 PM

--- stringjumble.py
+++ stringjumble.py
@@ -35,7 +35,6 @@ revcor = list(string)
 print(unedit)
 
 n = (len(unedit))
-a = n
 u = 0
 for b in range(0,n-1):
     if unedit[b] == ' ':
@@ -51,8 +50,8 @@ spaces = list[:]
 w = len(spaces)
 
 spaces.append(n)
-for t in range(1,w+1):
-    revcor[n-spaces[t-1]:n-spaces[t]] = unedit[spaces[t-1],spaces[t]]
+#for t in range(1,w+1):
+#    revcor[n-spaces[t-1]:n-spaces[t]] = unedit[spaces[t-1],spaces[t]]
 
 #for t in range(0,w):
     #list[n-spaces[t]] = unedit[0:spaces[t]]
